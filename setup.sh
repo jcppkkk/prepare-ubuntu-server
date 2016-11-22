64784293f50ba5013aa7177be9e8a968584c0433
Jethro Yu <jethro.yu@hopebaytech.com>
2016-11-22 04:21:58 +0000

    can set repo_url

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # curl -sL http://git.io/uinit | bash
 
+APT_REPO_URL=${1:-http://free.nchc.org.tw/ubuntu/}
+
 # allow local edit
 if [[ ! -f "$0" ]]; then
 	echo "Download uinit to PWD..."
@@ -17,7 +19,8 @@ if (( $(id -u) != 0 )); then
 fi
 
 set -xe
-{ echo "$0" "$@"; } 2>/dev/null
+{ printf '%-15s %s\n' COMMAND "$0 $@"; } 2>/dev/null
+{ printf '%-15s %s\n' APT_REPO_URL "$APT_REPO_URL"; } 2>/dev/null
 { echo "================================"; } 2>/dev/null
 
 export DEBIAN_FRONTEND=noninteractive
@@ -31,16 +34,15 @@ grep -q 'EDITOR=vim' ~/.bashrc || echo 'EDITOR=vim' >> ~/.bashrc
 
 if [ -e /etc/lsb-release ]; then
 	source /etc/lsb-release
-	SITE=http://free.nchc.org.tw/ubuntu/  
 	echo "# full list $(date --rfc-3339=seconds)
-	deb $SITE $DISTRIB_CODENAME main restricted universe multiverse
-	deb $SITE $DISTRIB_CODENAME-security main restricted universe multiverse
-	deb $SITE $DISTRIB_CODENAME-updates main restricted universe multiverse
-	deb $SITE $DISTRIB_CODENAME-backports main restricted universe multiverse
-	deb-src $SITE $DISTRIB_CODENAME main restricted universe multiverse
-	deb-src $SITE $DISTRIB_CODENAME-security main restricted universe multiverse
-	deb-src $SITE $DISTRIB_CODENAME-updates main restricted universe multiverse
-	deb-src $SITE $DISTRIB_CODENAME-backports main restricted universe multiverse
+	deb $APT_REPO_URL $DISTRIB_CODENAME main restricted universe multiverse
+	deb $APT_REPO_URL $DISTRIB_CODENAME-security main restricted universe multiverse
+	deb $APT_REPO_URL $DISTRIB_CODENAME-updates main restricted universe multiverse
+	deb $APT_REPO_URL $DISTRIB_CODENAME-backports main restricted universe multiverse
+	deb-src $APT_REPO_URL $DISTRIB_CODENAME main restricted universe multiverse
+	deb-src $APT_REPO_URL $DISTRIB_CODENAME-security main restricted universe multiverse
+	deb-src $APT_REPO_URL $DISTRIB_CODENAME-updates main restricted universe multiverse
+	deb-src $APT_REPO_URL $DISTRIB_CODENAME-backports main restricted universe multiverse
 	"| tee /etc/apt/sources.list;
 fi
 
